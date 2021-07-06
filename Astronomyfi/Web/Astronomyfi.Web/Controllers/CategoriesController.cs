167b03f58898dbbc922cac56d183081447e6c6a3
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-07-06 16:03:18 +0300

    Working on categories
    Implemented sample view page and base controller.

---
+++ Astronomyfi/Web/Astronomyfi.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿namespace Astronomyfi.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CategoriesController : BaseController
+    {
+        public IActionResult All()
+        {
+            return this.View();
+        }
+
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+    }
+}
