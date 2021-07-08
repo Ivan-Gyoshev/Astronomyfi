23b3c96f98749dff536014ae4f23369ae535791f
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-07-08 17:15:29 +0300

    Implementing categories view models

---
+++ Astronomyfi/Web/Astronomyfi.Web.ViewModels/Categories/AddCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Astronomyfi.Web.ViewModels.Categories
+{
+    public class AddCategoryViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Astronomyfi/Web/Astronomyfi.Web.ViewModels/Categories/ListCategoriesViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Astronomyfi.Web.ViewModels.Categories
+{
+    public class ListCategoriesViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int PostsCount { get; set; }
+    }
+}
