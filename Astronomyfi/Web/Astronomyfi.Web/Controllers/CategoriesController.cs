464abfcc126007ea0147737b3de47031d6e84104
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-07-08 17:16:04 +0300

    Finishing categories page

--- Astronomyfi/Web/Astronomyfi.Web/Controllers/CategoriesController.cs
+++ Astronomyfi/Web/Astronomyfi.Web/Controllers/CategoriesController.cs
@@ -1,12 +1,34 @@
 ﻿namespace Astronomyfi.Web.Controllers
 {
+    using System.Linq;
+
+    using Astronomyfi.Data;
+    using Astronomyfi.Web.ViewModels.Categories;
     using Microsoft.AspNetCore.Mvc;
 
     public class CategoriesController : BaseController
     {
+        private readonly ApplicationDbContext data;
+
+        public CategoriesController(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
         public IActionResult All()
         {
-            return this.View();
+            var categories = this.data.Categories
+                .Select(c => new ListCategoriesViewModel
+                {
+                    Name = c.Name,
+                    Description = c.Description,
+                    ImageUrl = c.ImageUrl,
+
+                    // TODO: Posts Count
+                })
+                .ToList();
+
+            return this.View(categories);
         }
 
         public IActionResult Add()
