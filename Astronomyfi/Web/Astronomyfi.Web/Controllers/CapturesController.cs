cb24dfe2a23c9fcf4ab02f863efc4f4ddddea4ff
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-07-24 19:26:39 +0300

    Working on Captures page pt1
    Introduced index page and sample listing page

---
+++ Astronomyfi/Web/Astronomyfi.Web/Controllers/CapturesController.cs
@@ -0,0 +1,11 @@
+﻿namespace Astronomyfi.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CapturesController : Controller
+    {
+        public IActionResult Index() => this.View();
+
+        public IActionResult All() => this.View();
+    }
+}
