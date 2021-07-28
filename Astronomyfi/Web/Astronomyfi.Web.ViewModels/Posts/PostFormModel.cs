993f1e52007a1cdec21f45f19ff753b0c8f2c5bb
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-07-28 16:36:53 +0300

    Exported post partial view

---
+++ Astronomyfi/Web/Astronomyfi.Web.ViewModels/Posts/PostFormModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Astronomyfi.Web.ViewModels.Posts
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Astronomyfi.Data.Models.Enums;
+
+    using static Astronomyfi.Data.Models.Common.DataConstants;
+
+    public class PostFormModel
+    {
+        [Required]
+        [StringLength(PostTitleMaxLength, MinimumLength = PostTitleMinLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(PostContentMaxLength, MinimumLength = PostContentMinLength)]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public TypeOfPost Type { get; set; }
+
+        public IEnumerable<PostCategoryViewModel> Categories { get; set; }
+
+        public IEnumerable<TypeOfPost> Types { get; set; }
+    }
+}
