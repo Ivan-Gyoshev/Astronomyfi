7c5332bca275555237179c9a2a24808ac9f7fd3e
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-08-07 14:13:04 +0300

    Seeded Forum Administrator

---
+++ Astronomyfi/Data/Astronomyfi.Data/Seeding/AdminSeeder.cs
@@ -0,0 +1,35 @@
+﻿namespace Astronomyfi.Data.Seeding
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Astronomyfi.Common;
+    using Astronomyfi.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class AdminSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            const string adminEmail = "admin@astronomyfi.com";
+            const string adminUserName = "AstroA";
+            const string adminPassword = "admin12";
+
+            var user = new ApplicationUser
+            {
+                Email = adminEmail,
+                UserName = adminUserName,
+            };
+
+            await userManager.CreateAsync(user, adminPassword);
+
+            await userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
+
+            await dbContext.SaveChangesAsync();
+
+        }
+    }
+}
--- Astronomyfi/Data/Astronomyfi.Data/Seeding/ApplicationDbContextSeeder.cs
+++ Astronomyfi/Data/Astronomyfi.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -27,6 +27,7 @@
                           {
                               new RolesSeeder(),
                               new CategoriesSeeder(),
+                              new AdminSeeder(),
                           };
 
             foreach (var seeder in seeders)
