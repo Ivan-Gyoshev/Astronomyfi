c167e0cb2d3fd71800fda47aa065b5934260ba17
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-08-07 14:29:02 +0300

    Disable the seeder once the user is seeded

--- Astronomyfi/Data/Astronomyfi.Data/Seeding/AdminSeeder.cs
+++ Astronomyfi/Data/Astronomyfi.Data/Seeding/AdminSeeder.cs
@@ -8,28 +8,25 @@
     using Microsoft.AspNetCore.Identity;
     using Microsoft.Extensions.DependencyInjection;
 
-    public class AdminSeeder : ISeeder
-    {
-        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
-        {
-            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    //public class AdminSeeder : ISeeder
+    //{
+    //    public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+    //    {
+    //        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-            const string adminEmail = "admin@astronomyfi.com";
-            const string adminUserName = "AstroA";
-            const string adminPassword = "admin12";
+    //        const string adminEmail = "admin@astronomyfi.com";
+    //        const string adminUserName = "AstroA";
+    //        const string adminPassword = "admin12";
 
-            var user = new ApplicationUser
-            {
-                Email = adminEmail,
-                UserName = adminUserName,
-            };
+    //        var user = new ApplicationUser
+    //        {
+    //            Email = adminEmail,
+    //            UserName = adminUserName,
+    //        };
 
-            await userManager.CreateAsync(user, adminPassword);
+    //        await userManager.CreateAsync(user, adminPassword);
 
-            await userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
-
-            await dbContext.SaveChangesAsync();
-
-        }
-    }
+    //        await userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
+    //    }
+    //}
 }
